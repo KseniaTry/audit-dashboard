de4fe0aa3e5010f2b6efe6de9d5773474659b598
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-09-06 15:08:35 +0300

    feat: add rendering Table annd Cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,21 +1,30 @@
 import { mockIssues } from "../mockData"
+import { useTranslation } from "react-i18next"
 
 const Cards = () => {
+    const { t } = useTranslation()
 
     return (
         mockIssues.map((issue) => {
-            return <div className='hidden md:grid w-max grid-cols-[80px_180px_120px_120px_120px_120px_120px_120px_120px] p-4 text-gray-700 border-b border-gray-200 text-sm'>
-                <div>{issue.id}</div>
-                <div>{issue.inspection}</div>
-                <div>{issue.auditViolation}</div>
-                <div>{issue.recommendation}</div>
-                <div>{issue.status}</div>
-                <div>{issue.riskLevel}</div>
-                <div>{issue.responsibleUnit}</div>
-                <div>{issue.responsiblePerson}</div>
-                <div>{issue.scheduledDate}</div>
-                <div>{issue.executionDate}</div>
-            </div>
+            return <div className='hidden break-words md:grid w-max grid-cols-[40px_120px_180px_180px_100px_100px_120px_120px_100px_100px] p-4 text-gray-700 border-b border-gray-200 text-sm'>
+                <div className="px-1 text-gray-400">{issue.id}</div>
+                <div className="px-1">{issue.inspection}</div>
+                <div className="px-1 text-xs">{issue.auditViolation}</div>
+                <div className="px-1 text-xs">{issue.recommendation}</div>
+                <div className="px-1">{t(`statuses.${issue.status}`)}</div>
+                <div className="px-1">{t(`riskLevels.${issue.riskLevel}`)}</div>
+                <div className="px-1">
+                    {issue.responsibleUnit.map((unit) => {
+                        return <div className="border border-gray-300 text-xs bg-[#F3F4F6] px-2 py-1 my-2 rounded">{unit}</div>
+                    })} </div>
+                <div className="px-1">
+                    {issue.responsiblePerson.map((person) => {
+                        return <div className="border border-gray-300 text-xs bg-[#F3F4F6] px-2 py-1 my-2 rounded">{person}</div>
+                    })}
+                </div>
+                <div className="px-1">{issue.scheduledDate}</div>
+                <div className="px-1">{issue.executionDate}</div>
+            </div >
         })
 
     )
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,23 +1,26 @@
 
 import Cards from "./Cards"
+import { useTranslation } from "react-i18next"
 
 const Table = () => {
+    const { t } = useTranslation()
+
     return (
         <>
             <section>
                 <div className="p-4 max-w-4xl mx-auto">
                     {/* заголовок таблицы */}
-                    <div className='hidden md:grid w-max grid-cols-[80px_180px_120px_120px_120px_120px_120px_120px_120px] bg-gray-100 p-4 rounded-t-xl font-semibold text-gray-700 border-b border-gray-200 text-sm'>
-                        <div>№</div>
-                        <div>Проверка</div>
-                        <div>Нарушение</div>
-                        <div>Рекомендация</div>
-                        <div>Статус</div>
-                        <div>Уровень риска</div>
-                        <div>Ответственное ССП</div>
-                        <div>Ответственное лицо</div>
-                        <div>Плановый срок исполнения</div>
-                        <div>Фактический срок исполнения</div>
+                    <div className='hidden break-words md:grid w-max grid-cols-[40px_120px_180px_180px_100px_100px_120px_120px_100px_100px] bg-gray-100 p-4 rounded-t-xl font-bold text-gray-700 border-b border-gray-200 text-[12px]'>
+                        <div className="px-1">{t('table.number')}</div>
+                        <div className="px-1">{t('table.inspection')}</div>
+                        <div className="px-1">{t('table.violation')}</div>
+                        <div className="px-1">{t('table.recommendation')}</div>
+                        <div className="px-1">{t('table.status')}</div>
+                        <div className="px-1">{t('table.riskLevel')}</div>
+                        <div className="px-1">{t('table.responsibleUnit')}</div>
+                        <div className="px-1">{t('table.responsiblePerson')}</div>
+                        <div className="px-1">{t('table.scheduledDate')}</div>
+                        <div className="px-1">{t('table.executionDate')}</div>
                     </div>
                     {/* карточки проверки 1 */}
                     <Cards />
