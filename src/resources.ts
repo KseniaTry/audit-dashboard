ce015ccc85e5944e4c7d097362ee70e76bce543a
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-09-06 14:30:13 +0300

    feat: add i18n recources and connection in main.js

---
+++ src/resources.ts
@@ -0,0 +1,30 @@
+export const resources = {
+  ru: {
+    translation: {
+      statuses: {
+        open: 'Не исполнено',
+        in_progress: 'В работе',
+        overdue: 'Просрочено',
+        closed: 'Исполнено',
+        removed: 'Снято',
+      },
+      riskLevels: {
+        high: 'Высокий',
+        medium: 'Средний',
+        low: 'Низкий',
+      },
+      table: {
+        number: '№',
+        inspection: 'Проверка',
+        violation: 'Нарушение',
+        recommendation: 'Рекомендация',
+        status: 'Статус',
+        riskLevel: 'Уровень риска',
+        responsibleUnit: 'Ответственное ССП',
+        responsiblePerson: 'Ответственное лицо',
+        scheduledDate: 'Плановая дата исполнения',
+        executionDate: 'Фактическая дата исполнения'
+      }
+    }
+  }
+}
