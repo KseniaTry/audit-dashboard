7d0ac0cf1a1db5d52398e97732764e4a8869e37f
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-09-12 14:20:44 +0300

    fix: refactor code in styles

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,21 +4,23 @@ import { useTranslation } from "react-i18next"
 const Cards = () => {
     const { t } = useTranslation()
 
-    const cardsClass = `grid grid-cols-4 gap-[10px] text-[10px] p-[15px] mb-[15px] my-[5px] w-full mx-auto bg-[#FFFFFF] 
+    const cardsClass = `
+            grid grid-cols-4 gap-[10px] text-[10px] p-[15px] mb-[15px] my-[5px] w-full mx-auto bg-[#FFFFFF] 
             border border-[#EAE9E2] shadow-sm break-words border-l-4 border-l-[#1E40AF]
-            md:grid-cols-[40px_120px_80px_80px_180px_180px_110px_100px_120px_120px_100px_100px] 
-            md:w-full md:max-w-none md:p-4 
-            md:m-0 md:text-xs md:text-gray-700 
-            md:rounded-none md:shadow-none md:bg-transparent
-            md:border-0 md:border-b md:border-gray-200 md:gap-0`
+            md:grid-cols-[40px_120px_80px_90px_180px_180px_110px_100px_120px_120px_100px_100px] 
+            md:gap-[5px] md:w-max md:max-w-none md:p-4 md:m-0 
+            md:text-xs md:text-gray-700 md:bg-white
+            md:rounded-none md:shadow-none md:border-l-[#1E40AF]
+         md:border-r-0 md:border-t md:border-b md:border-[#EAE9E2]`
     const labelClass = "md:hidden mb-[5px] text-[10px] text-[#9E9E9E] font-medium uppercase block tracking-wider"
+    const inspectionMobileWrapper = "flex items-baseline gap-2 col-start-1 col-end-4 row-start-1 md:contents"
+    const inspectionDatesClass = "flex gap-[5px] items-center"
     const inspectionClass = "md:text-[#1A1A1A] md:font-normal text-[#9E9E9E] text-[14px] font-semibold tracking-tight leading-snug"
     const recommendationClass = "mb-[5px] text-[13px] md:text-[12px] md:mb-0"
     const violationClass = "md:font-normal md:mb-0 font-bold mb-[5px] text-[12px]"
     const borderClass = "border-t border-[#EAE9E2] pt-4 md:border-none md:pt-0"
-    const baseMdClass = "md:col-auto md:row-auto"
-    const violationDatesClass = "flex gap-[5px]"
-    const buttonClass = "col-start-4 row-start-1 border border-[#1E40AF] rounded-lg font-bold w-[25px] h-[25px] ml-auto"
+    const resetMdClass = "md:col-auto md:row-auto"
+    const buttonClass = "md:hidden col-start-4 row-start-1 border border-[#1E40AF] rounded-lg font-bold w-[25px] h-[25px] ml-auto"
     // стили для рисков
     const riskLevelStyles = {
         high: "p-1 bg-red-50 text-red-700 border border-red-200 uppercase",
@@ -36,60 +38,63 @@ const Cards = () => {
     return (
         mockIssues.map((issue) => {
             return <div className={cardsClass}>
+                {/* кнопка "добавить" на мобильной версии */}
                 <button className={buttonClass}>+</button>
                 {/* обертка для карточки на мобилке (чтобы № и название проверки были вплотную) */}
-                <div className="flex items-baseline gap-2 col-start-1 col-end-4 row-start-1 md:contents">
-                    <div className="px-1 font-bold text-gray-400">
-                        <p className="md: hidden">{t('table.number')}</p>
-                        <p><span className="md:hidden">№</span>{issue.id}</p>
+                <div className={`${inspectionMobileWrapper}`}>
+                    <div className='font-bold text-gray-400'>
+                        <p><span className="md:hidden">{t('table.number')}</span>{issue.id}</p>
                     </div>
-                    <div className={`${baseMdClass} px-1`}>
+                    <div>
                         <p className={inspectionClass}>{issue.inspection}</p>
                     </div>
                 </div>
                 {/* остальные колонки */}
                 <div className="md:hidden col-start-1 col-end-2 row-start-2">
-                    <p className={labelClass}>{t('table.violationPeriod')}</p>
+                    <p className={labelClass}>{t('table.inspectionPeriod')}</p>
                 </div>
-                <div className={`${baseMdClass} ${violationDatesClass} px-1 col-start-2 row-start-2`}>
+                <div className={`${resetMdClass} ${inspectionDatesClass} justify-end col-start-2 row-start-2`}>
                     <p className="md:hidden">c</p>
                     <p>{issue.startInspectionDate}</p>
                 </div>
-                <div className={`${baseMdClass} ${violationDatesClass} px-1 m-auto col-start-3 row-start-2`}>
+                <div className={`${resetMdClass} ${inspectionDatesClass} justify-start m-auto col-start-3 row-start-2`}>
                     <p className="md:hidden">по</p>
                     <p>{issue.endInspectionDate === null ? '-' : issue.endInspectionDate}</p>
                 </div>
-                <div className={`${baseMdClass} px-1 col-start-1 col-end-5 row-start-3`}>
-                    <p className={violationClass}>{issue.auditViolation}</p></div>
-                <div className={`${borderClass} ${baseMdClass} ${recommendationClass} px-1 col-start-1 col-end-5 row-start-5`}>
+                <div className={`${resetMdClass} px-1 col-start-1 col-end-5 row-start-3`}>
+                    <p className={violationClass}>{issue.auditViolation}</p>
+                </div>
+                <div className={`${borderClass} ${resetMdClass} ${recommendationClass} col-start-1 col-end-5 row-start-5`}>
                     <p className={labelClass}>{t('table.recommendation')}</p>
                     <p>{issue.recommendation}</p>
                 </div>
-                <div className={`${baseMdClass} px-1 col-start-1 col-end-2 row-start-4`}>
+                <div className={`${resetMdClass} max-w-[100px] max-[400px]:col-end-3 col-start-1 col-end-2 row-start-4`}>
                     <p className={statusStyles[issue.status]}>{t(`statuses.${issue.status}`)}</p>
                 </div>
-                <div className={`${baseMdClass} px-1 col-start-2 col-end-3 row-start-4`}>
+                <div className={`${resetMdClass} max-w-[90px] max-[400px]:col-start-3 col-start-2 col-end-3 row-start-4`}>
                     <p className={riskLevelStyles[issue.riskLevel]}>{t(`riskLevels.${issue.riskLevel}`)}</p>
                 </div>
-                <div className={`${baseMdClass} px-1 col-start-1 col-end-3 row-start-6`}>
+                <div className={`${resetMdClass} col-start-1 col-end-3 row-start-6`}>
                     <p className={labelClass}>{t('table.responsibleUnit')}</p>
                     {issue.responsibleUnit.map((unit) => {
                         return <p className="text-xs md:mb-2">{unit}</p>
-                    })} </div>
-                <div className={`${baseMdClass} px-1 col-start-3 col-end-5 row-start-6`}>
+                    })}
+                </div>
+                <div className={`${resetMdClass} col-start-3 col-end-5 row-start-6`}>
                     <p className={labelClass}>{t('table.responsiblePerson')}</p>
                     {issue.responsiblePerson.map((person) => {
                         return <div className="text-xs md:mb-2">{person}</div>
                     })}
                 </div>
-                <div className={`${baseMdClass} px-1 col-start-1 col-end-3 row-start-7`}>
+                <div className={`${resetMdClass} col-start-1 col-end-3 row-start-7`}>
                     <p className={labelClass}>{t('table.scheduledDate')}</p>
                     <p> {issue.scheduledDate}</p>
                 </div>
-                <div className={`${baseMdClass} px-1 col-start-3 col-end-5 row-start-7`}>
+                <div className={`${resetMdClass} col-start-3 col-end-5 row-start-7`}>
                     <p className={labelClass}>{t('table.executionDate')}</p>
-                    <p>{issue.executionDate === null ? '-' : issue.executionDate}</p></div>
-            </div >
+                    <p>{issue.executionDate === null ? '-' : issue.executionDate}</p>
+                </div>
+            </div>
         })
 
     )
