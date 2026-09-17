04ebac9ff162233b8842bc661a0cc3497d531e50
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-09-17 14:13:28 +0300

    fix: add correct reset button positioning

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -14,10 +14,10 @@ const Filters = ({ inspections }: FiltersProps) => {
     const buttonsClass = "text-[#9E9E9E] hover:border hover:border-2 hover:border-[#1E40AF]"
     const activeButtonClass = "bg-[#1E40AF] text-white"
     const selectClass = "max-[768px]:w-full h-[36px]"
-    const formClass = "flex p-4 gap-4 flex-wrap justify-between items-center tracking-widest text-[10px] md:text-[12px]"
+    const formClass = "relative flex p-4 gap-4 flex-wrap justify-between items-center tracking-widest text-[10px] md:text-[12px]"
     const buttonsWrapperClass = "flex w-full gap-2 flex-wrap whitespace-normal"
     const selectsWrapperClass = "flex w-full gap-2 justify-start items-center flex-wrap"
-    const resetButtonWrapperClass = "absolute right-10 top-6 -translate-y-1/2 px-2 h-[14px] flex items-center"
+    const resetButtonWrapperClass = "absolute right-0 top-[-15px] -translate-y-1/2 px-2 h-[14px] flex items-center"
     const resetButtonClass = `flex items-center gap-1.5 px-3 py-1.5 text-xs font-semibold uppercase tracking-widest text-[11px]
         text-[#9E9E9E] hover:text-[#1E40AF] transition-colors duration-150 cursor-pointer
         ml-auto max-[768px]:w-full max-[768px]:justify-center max-[768px]:border max-[768px]:border-1 max-[768px]:border-[#1E40AF] 
