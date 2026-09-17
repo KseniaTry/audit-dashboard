f0228422aa79854b2e40da9c341b920022c0e606
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-09-17 13:48:47 +0300

    feat: connect Analytics at App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import Table from './components/Table'
 import Filters from './components/Filters'
-import { mockIssues } from './mockData'
+import { mockIssues, RISK_LEVELS, STATUSES } from './mockData'
+import Analytics from './components/Analytics'
 
 function App() {
 
   return (
     <>
+    <Analytics riskLevels={RISK_LEVELS} statuses={STATUSES} years={[2023, 2024, 2025, 2026]} />
     <Filters inspections={mockIssues} />
     <Table />
     </>
