d354363e86dc75edd354d8696e633be8da9d1ff5
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-09-19 13:42:46 +0300

    feat: add icons components (svg)

---
+++ src/components/icons/PlusIcon.tsx
@@ -0,0 +1,23 @@
+
+interface ClassName {
+    className: string
+}
+
+const PlusIcon = ({ className }: ClassName) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={800}
+        height={800}
+        fill="none"
+        viewBox="0 0 24 24"
+        className={className}
+    >
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M11.25 12.75V18h1.5v-5.25H18v-1.5h-5.25V6h-1.5v5.25H6v1.5h5.25Z"
+            clipRule="evenodd"
+        />
+    </svg>
+)
+export default PlusIcon
