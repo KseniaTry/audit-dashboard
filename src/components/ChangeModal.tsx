053199b33e43b90775e40184bda1ade6e07f4548
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-09-23 11:39:55 +0300

    fix: add styles for desktop

--- src/components/ChangeModal.tsx
+++ src/components/ChangeModal.tsx
@@ -6,7 +6,7 @@ const ChangeModal = () => {
     const { t } = useTranslation()
     const headerClass = "bg-[#F4F3EF] p-4 border-b border-b-[#EAE9E2]"
     const closeButtonClass = "flex items-center justify-center w-10 h-10 cursor-pointer"
-    const headerTitleClass = "text-[20px] w-full max-[768px]:text-[23px]"
+    const headerTitleClass = "w-full text-[23px]"
     const headerWrapperClass = "flex flex-wrap items-center justify-between gap-3 md:gap-2"
     const wrapperClass = "flex flex-col flex-wrap gap-2"
     const borderClass = "border border-[#EAE9E2] bg-[#F4F3EF] p-4"
@@ -18,16 +18,16 @@ const ChangeModal = () => {
         removed: "p-1 bg-gray-100 text-gray-500 line-through-none uppercase",
         overdue: "p-1 bg-rose-600 text-white border border-rose-300 uppercase"
     }
-    const buttonBaseClass = "flex items-center gap-1 p-2 border border-black w-max uppercase tracking-widest text-[12px] hover:bg-[#9E9E9E] hover:border-[#9E9E9E] hover:text-white transition-colors cursor-pointer"
+    const buttonBaseClass = "flex items-center gap-1 p-2 border border-black w-max uppercase tracking-widest text-[14px] hover:bg-[#9E9E9E] hover:border-[#9E9E9E] hover:text-white transition-colors cursor-pointer"
 
     return (
         <div className={`flex fixed inset-0 z-50 justify-center md:items-center md:p-4 bg-black/50 backdrop-blur-sm`}>
-            <div className="w-full h-full flex flex-col justify-between md:w-[50%] md:h-[90%] bg-white border rounded-xl overflow-y-auto">
+            <div className="w-full h-full flex flex-col justify-between md:w-[50%] md:h-max bg-white border rounded-xl overflow-y-auto">
                 {/* ШАПКА */}
                 <header className={headerClass}>
                     <div className={headerWrapperClass}>
                         <div>
-                            <p className="tracking-widest uppercase text-[10px] text-[#1E40AF] font-semibold">{t('changeModal.edit')}</p>
+                            <p className="tracking-widest uppercase text-[10px] md:text-[11px] text-[#1E40AF] font-semibold">{t('changeModal.edit')}</p>
                             <h1 className={headerTitleClass}>{t('addModal.title')}</h1>
                         </div>
                         <button className={closeButtonClass} type="button">
@@ -36,11 +36,11 @@ const ChangeModal = () => {
                     </div>
                 </header>
                 {/* ФОРМА */}
-                <form className="flex flex-col gap-3 p-5 h-[100%]">
+                <form className="flex flex-col gap-3 p-5 h-full">
                     {/* НАЗВАНИЕ ПРОВЕРКИ */}
-                    <div className={borderClass}>
+                    <div className={twMerge(borderClass, 'flex flex-col gap-1')}>
                         <p className={titleClass}>{t('table.inspection')}</p>
-                        <p>Название текущей проверки</p>
+                        <p>{t('table.number')} Номер проверки - Название текущей проверки</p>
                     </div>
                     {/* ПЛАНОВАЯ ДАТА (ДЛЯ ИНФО - ТОЛЬКО ДЛЯ ЧТЕНИЯ) */}
                     <div className={twMerge(wrapperClass, 'w-auto')}>
@@ -58,10 +58,10 @@ const ChangeModal = () => {
                         <label className={twMerge('flex justify-start items-center flex-wrap cursor-pointer')} htmlFor="removed-switch" >
                             <input className="sr-only peer" id="removed-switch" name="removed-switch" type="checkbox"></input>
                             <div className={`relative border w-[49px] h-[23px] rounded-xl border-[#EAE9E2] bg-[#EAE9E2] 
-                               peer-checked:bg-green-500 peer-checked:border-green-500 transition-colors duration-200
+                               peer-checked:bg-[#1E40AF] peer-checked:border-[#1E40AF] transition-colors duration-200
                                 after:content-[''] after:absolute after:top-[1px] after:left-[2px] after:w-[20px] after:h-[19px] after:bg-white after:border after:rounded-full after:border-white
                                  peer-checked:after:translate-x-6 after:transition-transform after:duration-200`}></div>
-                            <span className="ml-5">{t('changeModal.switchToRemoved')}</span>
+                            <span className="ml-5 text-[#9E9E9E] tracking-wide font-semibold ">{t('changeModal.switchToRemoved')}</span>
                             {/* добавить смену текста если выбран чекбокс */}
                         </label>
                     </div>
